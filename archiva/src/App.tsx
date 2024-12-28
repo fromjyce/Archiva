cbd0cac6622c2c065b4038f1436797e3e97832b7
Jayashre <jaya2004kra@gmail.com>
2024-12-29 01:52:24 +0530

    added

--- archiva/src/App.tsx
+++ archiva/src/App.tsx
@@ -1,12 +1,28 @@
-import './App.css'
+import './App.css';
+import { FaApple } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to My Extension</h1>
-      <p>Connected Wallet: None</p>
+    <div className="popup-container">
+      <img src="logo.png" alt="Archiva Logo" className="logo" />
+      <h1 className="title">Archiva</h1>
+      <p className="connect-text">Connect with</p>
+      <div className="button-container">
+        <button className="auth-button google">
+          <FcGoogle className="icon" />
+          Google
+        </button>
+        <button className="auth-button apple">
+          <FaApple className="icon" />
+          Apple
+        </button>
+      </div>
+      <p className="support-text">
+        Need Help? <a href="https://support.archiva.com" className="support-link">Contact Archiva Support</a>
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
